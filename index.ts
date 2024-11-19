399bb50fc622d62f4909c30d41863938627af0ef
Moattar Ansari <moattaransari62@gmail.com>
2024-11-19 07:49:21 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,4 @@
+                                     //explicitly ....
+
+// TypeScript mein variable ya function declare karte waqt, agar humne explicitly (zahiran) type nahi diya hota to TypeScript khud se inferred (samajh kar) type determine kar leta hai. Yaad rahe, ismein humne type ko nahi declare kiya hota, lekin TypeScript khud samajh leta hai ke variable ya function ka type kya hona chahiye based on uski value ya return type.
+
